4ee9bd09781a6a10bc126c5a7ffeed1328e29ad6
Anastasya <bran0392@github.com>
2020-12-11 15:06:10 +0000

    Fixed width and height on Css for the img carousel. Worked together on the pause button with Chisha, however its still not working. Trying to figure out on what is the problem.

--- app.js
+++ app.js
@@ -24,27 +24,64 @@ tabs.forEach(tab => {
   
 })
 
-// Image Carousel
+            // Image Carousel
+            var slideIndex = 0;
+            var timer = setInterval(showAllSlides, 3000);
+            const pictures = document.getElementsByClassName("slides"); //grab slide
 
-//Automatic Play
-var slideIndex = 0;
-var timer = setInterval(showAllSlides, 3000);
+            console.log(pictures);
 
-function showAllSlides(){
-  var i; //iterator
-  var picture = document.getElementsByClassName("slides"); //grab slide
-  
-  for(i=0; i < picture.length; i++){
-    picture[i].style.display= "none"; //hidding the image that is not iterated on
-    console.log(picture);
-  }
+            //Automatic Play
+            function showAllSlides(){
+              for(let i = 0; i < pictures.length; i++){
+                pictures[i].style.display= "none"; //hidding the image that is not iterated on
+              }
 
-  slideIndex++;
-  if(slideIndex > picture.length){
-    slideIndex = 1;
-  }
-  picture[slideIndex-1].style.display = "block";
+                    slideIndex++;
 
-  
+              if(slideIndex > pictures.length){
+                slideIndex = 1;
+              }
+              pictures[slideIndex-1].style.display = "block";
+            }
 
-}
+            function showStop(){
+              clearInterval(timer);
+            }
+
+            function restartShow(){
+              timer = setInterval(showAllSlides, 3000);
+            }
+
+            pictures[0].addEventListener("mouseover", showStop);
+            pictures[0].addEventListener("mouseleave", restartShow);
+
+
+ //PAUSE BUTTON FUNCTION
+            var pauseButton = document.getElementById("pause");          //Grab pause button
+            let play = true;
+            function stopStart(){
+              if (play){
+                clearInterval(timer); 
+                pauseButton.innerHTML = "Play me";
+                play = false; 
+              } else {
+                play = true;
+                timer = setInterval(showAllSlides, 4000);
+                pauseButton.innerHTML = "Pause me";
+              }
+            }
+            pauseButton.addEventListener("click", function(){
+            stopStart();
+            });
+
+  //Next & Previous Button
+
+            let next = () => {
+              slideIndex >= pictures.length-1 ? slideIndex = 0 : slideIndex++
+              showAllSlides(slideIndex)
+            }
+
+          document.querySelector(".fas fa-arrow-left").addEventListener('click', next())
+          console.log(next);
+            
